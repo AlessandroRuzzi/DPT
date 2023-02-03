eaab59cab8b0ce2729a6226ced06e30b0f4a34ae
AlessandroRuzzi <alrxruzzi98@gmail.com>
2023-02-03 18:02:18 +0100

    u

--- yolov6/core/inferer.py
+++ yolov6/core/inferer.py
@@ -21,6 +21,56 @@ from yolov6.utils.nms import non_max_suppression
 from yolov6.utils.torch_utils import get_model_info
 from operator import itemgetter
 
+def get_iou(bb1, bb2):
+    """
+    Calculate the Intersection over Union (IoU) of two bounding boxes.
+
+    Parameters
+    ----------
+    bb1 : dict
+        Keys: {'x1', 'x2', 'y1', 'y2'}
+        The (x1, y1) position is at the top left corner,
+        the (x2, y2) position is at the bottom right corner
+    bb2 : dict
+        Keys: {'x1', 'x2', 'y1', 'y2'}
+        The (x, y) position is at the top left corner,
+        the (x2, y2) position is at the bottom right corner
+
+    Returns
+    -------
+    float
+        in [0, 1]
+    """
+    assert bb1['x1'] < bb1['x2']
+    assert bb1['y1'] < bb1['y2']
+    assert bb2['x1'] < bb2['x2']
+    assert bb2['y1'] < bb2['y2']
+
+    # determine the coordinates of the intersection rectangle
+    x_left = max(bb1['x1'], bb2['x1'])
+    y_top = max(bb1['y1'], bb2['y1'])
+    x_right = min(bb1['x2'], bb2['x2'])
+    y_bottom = min(bb1['y2'], bb2['y2'])
+
+    if x_right < x_left or y_bottom < y_top:
+        return 0.0
+
+    # The intersection of two axis-aligned bounding boxes is always an
+    # axis-aligned bounding box
+    intersection_area = (x_right - x_left) * (y_bottom - y_top)
+
+    # compute the area of both AABBs
+    bb1_area = (bb1['x2'] - bb1['x1']) * (bb1['y2'] - bb1['y1'])
+    bb2_area = (bb2['x2'] - bb2['x1']) * (bb2['y2'] - bb2['y1'])
+
+    # compute the intersection over union by taking the intersection
+    # area and dividing it by the sum of prediction + ground-truth
+    # areas - the interesection area
+    iou = intersection_area / float(bb1_area + bb2_area - intersection_area)
+    assert iou >= 0.0
+    assert iou <= 1.0
+    return iou
+
 class Inferer:
     def __init__(self, source, webcam, webcam_addr, weights, device, yaml, img_size, half):
 
@@ -120,7 +170,7 @@ class Inferer:
                             human_center = [(xyxy[0]+xyxy[2])/2, (xyxy[1] + xyxy[3])/2]
                             human_corners = xyxy
                         else:
-                            object_center_list.append(([(xyxy[0]+xyxy[2])/2, (xyxy[1] + xyxy[3])/2], label, xyxy))
+                            object_center_list.append(([(xyxy[0]+xyxy[2])/2, (xyxy[1] + xyxy[3])/2], label, xyxy, get_iou(human_corners, xyxy)))
                         
                         #with open(txt_path + '.txt', 'a') as f:
                         #    f.write(('%g ' * len(line)).rstrip() % line + '\n')
@@ -134,9 +184,9 @@ class Inferer:
                 for i, center in enumerate(object_center_list):
                     class_num = int(cls)
         
-                    object_dist_list.append(math.dist(human_center,center[0]))
+                    object_dist_list.append(object_center_list[3])
 
-                pos, element = min(enumerate(object_dist_list), key=itemgetter(1))
+                pos, element = max(enumerate(object_dist_list), key=itemgetter(1))
 
                 img_src = np.asarray(img_ori)
 
